932d5c0410326bf2b279e16bbed44985eef53c59
Sylvain Just <sy.just@i-sloth.com>
2021-11-24 16:17:53 +0100

    build: update todo / docker-build doc script

--- bin/docker-build.sh
+++ bin/docker-build.sh
@@ -3,18 +3,22 @@ export ENV="dev"
 export DOCKER_BUILDKIT=0
 docker-composer up --build
 exit
+docker-compose exec web /bin/bash
+# run following scripts into container
+
+# Maybe useless
 # TODO: Fix composer home repo.magento.com credentials => auth.json
 #       Works fine with http://user:password@repo.magento.com directly
 #       into /var/www/html/composer.json repository section (container context)
 
-# run following scripts into container
 composer require alma/alma-php-client
 #rm -r app/code/Alma/MonthlyPayments/vendor
-docker-compose exec web /usr/local/bin/install-magento
-docker-compose exec web /usr/local/bin/fix-permissions
-docker-compose exec web /usr/local/bin/install-sampledata
+/usr/local/bin/install-magento
+/usr/local/bin/fix-permissions
+/usr/local/bin/install-sampledata
 
-# build mangento env as www-data user (todo: move this instructions under /usr/local/bin/script like 3 previous ones)
+# build magento env as www-data user (todo: move this instructions under /usr/local/bin/script like 3 previous ones)
+su www-data
 bin/magento cache:clean
 bin/magento setup:upgrade
 bin/magento setup:di:compile # compile dependence injections
