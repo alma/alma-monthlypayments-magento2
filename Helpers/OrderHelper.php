1223666140144fc10ca63beb0407c16821ae5394
Francois Gomis <francois.gomis@getalma.eu>
2022-06-03 11:51:20 +0200

    feat: add transaction to invoice

--- Helpers/OrderHelper.php
+++ Helpers/OrderHelper.php
@@ -5,6 +5,9 @@ namespace Alma\MonthlyPayments\Helpers;
 use Magento\Framework\App\Helper\AbstractHelper;
 use Magento\Framework\App\Helper\Context;
 use Magento\Sales\Api\Data\OrderInterface;
+use Magento\Sales\Api\OrderManagementInterface;
+use Magento\Sales\Api\OrderRepositoryInterface;
+use Magento\Sales\Model\Order;
 use Magento\Sales\Model\OrderFactory;
 
 class OrderHelper extends AbstractHelper
@@ -13,23 +16,66 @@ class OrderHelper extends AbstractHelper
      * @var OrderFactory
      */
     private $orderFactory;
+    /**
+     * @var OrderManagementInterface
+     */
+    private $orderManagement;
+    /**
+     * @var OrderRepositoryInterface
+     */
+    private $orderRepository;
 
     public function __construct(
         Context $context,
-        OrderFactory $orderFactory
-    )
-    {
+        OrderFactory $orderFactory,
+        OrderRepositoryInterface $orderRepository,
+        OrderManagementInterface $orderManagement
+    ) {
         parent::__construct($context);
         $this->orderFactory = $orderFactory;
+        $this->orderManagement = $orderManagement;
+        $this->orderRepository = $orderRepository;
     }
 
     /**
-     * @param int $orderId
+     * @param string $orderId
      * @return OrderInterface
      */
-    public function getOrderById($orderId):OrderInterface
+    public function getOrderById(string $orderId): OrderInterface
     {
         $orderModel = $this->orderFactory->create();
         return $orderModel->loadByIncrementId($orderId);
     }
+
+    /**
+     * @param string $orderId
+     *
+     * @return void
+     */
+    public function cancelOrderById(string $orderId): void
+    {
+        $this->orderManagement->cancel($orderId);
+    }
+
+    /**
+     * @param string $orderId
+     *
+     * @return void
+     */
+    public function notifyOrderById(string $orderId): void
+    {
+        $this->orderManagement->notify($orderId);
+    }
+
+    /**
+     * @param Order $order
+     *
+     * @return void
+     */
+    public function saveOrderInRepository(Order $order): void
+    {
+        $this->orderRepository->save($order);
+    }
+
+
 }
--- Test/Unit/Helpers/OrderHelperTest.php
+++ Test/Unit/Helpers/OrderHelperTest.php
@@ -4,6 +4,9 @@ namespace Alma\MonthlyPayments\Test\Unit\Helpers;
 
 use Alma\MonthlyPayments\Helpers\OrderHelper;
 use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Sales\Api\OrderManagementInterface;
+use Magento\Sales\Api\OrderRepositoryInterface;
+use Magento\Sales\Model\Order;
 use Magento\Sales\Model\OrderFactory;
 use PHPUnit\Framework\TestCase;
 use Magento\Framework\App\Helper\Context;
@@ -14,23 +17,67 @@ class OrderHelperTest extends TestCase
      * @var OrderHelper
      */
     private $orderHelper;
+    /**
+     * @var OrderRepositoryInterface|\PHPUnit\Framework\MockObject\MockObject
+     */
+    private $orderRepository;
+    /**
+     * @var OrderFactory|\PHPUnit\Framework\MockObject\MockObject
+     */
+    private $orderFactory;
 
     protected function setUp(): void
     {
-        $contextMock = $this->createMock(Context::class);
-        $orderFactory = $this->createMock(OrderFactory::class);
-        $this->orderHelper = new OrderHelper($contextMock,$orderFactory);
+        $this->contextMock = $this->createMock(Context::class);
+        $this->orderFactory = $this->createMock(OrderFactory::class);
+        $this->orderRepository = $this->createMock(OrderRepositoryInterface::class);
+        $this->orderManagement = $this->createMock(OrderManagementInterface::class);
     }
 
     public function testInstanceConfigHelper()
     {
-        $this->assertInstanceOf(OrderHelper::class,$this->orderHelper);
+        $orderHelper = $this->createNewOrderHelper();
+        $this->assertInstanceOf(OrderHelper::class, $orderHelper);
     }
 
     public function testImplementAbstractHelper()
     {
-        $this->assertInstanceOf(AbstractHelper::class,$this->orderHelper);
+        $orderHelper = $this->createNewOrderHelper();
+        $this->assertInstanceOf(AbstractHelper::class, $orderHelper);
     }
 
+    public function testCancelOrderUseOrderId()
+    {
+        $mockOrderId = 10;
+        $this->orderManagement->expects($this->once())
+            ->method('cancel')
+            ->with($mockOrderId);
+        $orderHelper = $this->createNewOrderHelper();
+        $orderHelper->cancelOrderById($mockOrderId);
+    }
+    public function testNotifyOrderUseOrderId()
+    {
+        $mockOrderId = 10;
+        $this->orderManagement->expects($this->once())
+            ->method('notify')
+            ->with($mockOrderId);
+        $orderHelper = $this->createNewOrderHelper();
+        $orderHelper->notifyOrderById($mockOrderId);
+    }
+
+    public function testSaveMethodStructure(): void
+    {
+        $mockOrder = $this->createMock(Order::class);
+        $this->orderRepository->expects($this->once())
+            ->method('save')
+            ->with($mockOrder);
+        $orderHelper = $this->createNewOrderHelper();
+        $orderHelper->saveOrderInRepository($mockOrder);
+    }
+
+    private function createNewOrderHelper(): OrderHelper
+    {
+        return new OrderHelper($this->contextMock, $this->orderFactory, $this->orderRepository, $this->orderManagement);
+    }
 
 }
