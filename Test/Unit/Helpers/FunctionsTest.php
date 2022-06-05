f8c54db1a8ae80c60d1461175de2a2833eb43fe4
Francois Gomis <francois.gomis@getalma.eu>
2022-06-05 18:10:39 +0200

    test : Add test on price to cent

---
+++ Test/Unit/Helpers/FunctionsTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Alma\MonthlyPayments\Test\Unit\Helpers;
+
+use Alma\MonthlyPayments\Helpers\Functions;
+use PHPUnit\Framework\TestCase;
+
+class FunctionsTest extends TestCase
+{
+
+    /**
+     * @dataProvider priceDataProvider
+     */
+    public function testConvertPriceToCent($price, $result): void
+    {
+        $this->assertEquals($result, Functions::priceToCents($price));
+    }
+
+    public function priceDataProvider(): array
+    {
+        return [
+            'Test with int Price' => [
+                'price' => 45,
+                'result' => 4500
+            ],
+            'Test with string price' => [
+                'price' => '42',
+                'result' => 4200
+            ],
+            'Test with negative price' => [
+                'price' => -55,
+                'result' => -5500
+            ],
+            'Test with float price' => [
+                'price' => 12.13,
+                'result' => 1213
+            ],
+            'Test with string float price' => [
+                'price' => '12.12',
+                'result' => 1212
+            ],
+            'Test with string negative float price' => [
+                'price' => '-13.12',
+                'result' => -1312
+            ],
+        ];
+    }
+
+}
