793310e53cd14056daa5332bdc583ad4af0b5288
Francois Gomis <francois.gomis@getalma.eu>
2022-06-06 23:15:23 +0200

    feat : add total_refund and order total in data Builder

--- Gateway/Request/RefundDataBuilder.php
+++ Gateway/Request/RefundDataBuilder.php
@@ -3,15 +3,10 @@
 namespace Alma\MonthlyPayments\Gateway\Request;
 
 use Alma\MonthlyPayments\Gateway\Config\Config;
-use Alma\MonthlyPayments\Helpers\Logger;
 use Magento\Payment\Gateway\Data\PaymentDataObject;
 use Magento\Payment\Gateway\Helper\SubjectReader;
 use Magento\Payment\Gateway\Request\BuilderInterface;
-use Magento\Payment\Model\InfoInterface;
-use Magento\Sales\Api\Data\OrderInterface;
-use Magento\Sales\Model\Order\Creditmemo;
 use Magento\Sales\Model\OrderRepository;
-use Magento\Sales\Model\ResourceModel\Order\Creditmemo\Collection as CreditmemoCollection;
 
 class RefundDataBuilder implements BuilderInterface
 {
@@ -25,22 +20,16 @@ class RefundDataBuilder implements BuilderInterface
      * @var OrderRepository
      */
     private $orderRepository;
-    /**
-     * @var Logger
-     */
-    private $logger;
 
     /**
      * @param Config $config
      */
     public function __construct(
-        Logger $logger,
         Config $config,
         OrderRepository $orderRepository
     ) {
         $this->config = $config;
         $this->orderRepository = $orderRepository;
-        $this->logger = $logger;
     }
 
     /**
@@ -55,11 +44,10 @@ class RefundDataBuilder implements BuilderInterface
         $payment   = $paymentDO->getPayment();
         $orderDO   = $paymentDO->getOrder();
         $order = $this->orderRepository->get($orderDO->getId());
-        /** @var InfoInterface $payment */
         $refundPayload['payment_id'] =  $payment->getAdditionalInformation(Config::ORDER_PAYMENT_ID);
         $refundPayload['merchant_id'] = $this->config->getMerchantId();
         $refundPayload['amount'] = $buildSubject['amount'];
-        $refundPayload['total_refund'] = $this->getTotalRefunded();
+        $refundPayload['total_refund'] = $order->getTotalRefunded();
         $refundPayload['order_total'] = $order->getGrandTotal();
         return $refundPayload;
     }
