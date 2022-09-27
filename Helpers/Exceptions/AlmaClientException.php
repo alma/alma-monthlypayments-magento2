23a03d3cf3713a5e44136f23da28032c0543212f
Francois Gomis <francois.gomis@getalma.eu>
2022-09-27 15:30:18 +0200

    fix : add consent API

--- Helpers/Exceptions/AlmaClientException.php
+++ Helpers/Exceptions/AlmaClientException.php
@@ -6,5 +6,13 @@ use Exception;
 
 class AlmaClientException extends Exception
 {
-
+    /**
+     * Get exception message same as Request error
+     *
+     * @return string
+     */
+    public function getErrorMessage(): string
+    {
+        return $this->getMessage();
+    }
 }
---
+++ Model/Adminhtml/Config/SOCFieldSave.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace Alma\MonthlyPayments\Model\Adminhtml\Config;
+
+use Alma\API\RequestError;
+use Alma\MonthlyPayments\Helpers\AlmaClient;
+use Alma\MonthlyPayments\Helpers\Exceptions\AlmaClientException;
+use Alma\MonthlyPayments\Helpers\Logger;
+use Alma\MonthlyPayments\Helpers\ShareOfCheckout\SOCHelper;
+use Magento\Framework\App\Cache\TypeListInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\App\Config\Value;
+use Magento\Framework\Data\Collection\AbstractDb;
+use Magento\Framework\Message\ManagerInterface;
+use Magento\Framework\Model\Context;
+use Magento\Framework\Model\ResourceModel\AbstractResource;
+use Magento\Framework\Registry;
+
+class SOCFieldSave extends Value
+{
+
+    /**
+     * @var SOCHelper
+     */
+    private $socHelper;
+    /**
+     * @var AlmaClient
+     */
+    private $almaClient;
+    /**
+     * @var Logger
+     */
+    private $logger;
+    /**
+     * @var ManagerInterface
+     */
+    private $messageManager;
+
+    /**
+     * @param SOCHelper $socHelper
+     * @param Context $context
+     * @param Registry $registry
+     * @param ScopeConfigInterface $config
+     * @param TypeListInterface $cacheTypeList
+     * @param AlmaClient $almaClient
+     * @param Logger $logger
+     * @param ManagerInterface $messageManager
+     * @param AbstractResource|null $resource
+     * @param AbstractDb|null $resourceCollection
+     */
+    public function __construct(
+        SOCHelper            $socHelper,
+        Context              $context,
+        Registry             $registry,
+        ScopeConfigInterface $config,
+        TypeListInterface    $cacheTypeList,
+        AlmaClient           $almaClient,
+        Logger               $logger,
+        ManagerInterface     $messageManager,
+        AbstractResource     $resource = null,
+        AbstractDb           $resourceCollection = null
+    ) {
+        parent::__construct($context, $registry, $config, $cacheTypeList, $resource, $resourceCollection, []);
+        $this->socHelper = $socHelper;
+        $this->almaClient = $almaClient;
+        $this->logger = $logger;
+        $this->messageManager = $messageManager;
+    }
+
+    /**
+     * Send consent to API
+     *
+     * @return SOCFieldSave
+     */
+    public function beforeSave(): SOCFieldSave
+    {
+        if (!$this->isValueChanged()) {
+            return $this;
+        }
+        $value = $this->getValue();
+
+        $endpoint = 'addConsent';
+        if ($value == SOCHelper::SELECTOR_NO) {
+            $endpoint = 'removeConsent';
+        }
+        $this->sendConsent($endpoint);
+        return parent::beforeSave();
+    }
+
+    /**
+     * Save consent date after save
+     *
+     * @return SOCFieldSave
+     */
+    public function afterSave()
+    {
+        if ($this->isValueChanged() && $this->getValue()) {
+            $this->socHelper->saveDate(date('Y-m-d'));
+        } elseif ($this->isValueChanged() && !$this->getValue()) {
+            $this->socHelper->deleteDate();
+        }
+        return parent::afterSave();
+    }
+    /**
+     * Change $this->_dataSaveAllowed flag to "false" for disallow save
+     *
+     * @return void
+     */
+    protected function disallowDataSave(): void
+    {
+        $this->_dataSaveAllowed = false;
+    }
+
+    /**
+     * Send consent to alma API  - No save if an exception is trowed
+     *
+     * @param string $endpoint
+     *
+     * @return void
+     */
+    protected function sendConsent(string $endpoint): void
+    {
+        try {
+            $this->almaClient->getDefaultClient()->shareOfCheckout->$endpoint();
+        } catch (RequestError | AlmaClientException $e) {
+            $this->logger->error('SOC before save exception', [$e]);
+            $this->messageManager->addErrorMessage(
+                __('Impossible to save merchant data sharing settings, please try again later')
+            );
+            $this->disallowDataSave();
+        }
+    }
+}
