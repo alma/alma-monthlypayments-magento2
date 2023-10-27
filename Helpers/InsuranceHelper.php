70dc27731c9444967eeed67ff2dfe115f857f7f9
Francois-Gomis <francois.gomis@getalma.eu>
2023-10-27 10:46:49 +0200

    feature: create insurance helper for new quote_item property

---
+++ Helpers/InsuranceHelper.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Alma\MonthlyPayments\Helpers;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Framework\App\Helper\Context;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\Serialize\Serializer\Json;
+use Magento\Quote\Model\Quote\Item;
+
+class InsuranceHelper extends AbstractHelper
+{
+    /**
+     * @var Json
+     */
+    private $json;
+    /**
+     * @var RequestInterface
+     */
+    private $request;
+
+    /**
+     * @param Context $context
+     * @param Json $json
+     */
+    public function __construct(
+        Context $context,
+        RequestInterface $request,
+        Json $json
+    ) {
+        parent::__construct($context);
+        $this->json = $json;
+        $this->request = $request;
+    }
+
+    /**
+     * Get alma_insurance data from model
+     *
+     * @param Item $quoteItem
+     * @return string
+     */
+    public function getQuoteItemAlmaInsurance(Item $quoteItem): ?string
+    {
+        return $quoteItem->getAlmaInsurance();
+    }
+
+    /**
+     * Set alma_insurance in DB
+     *
+     * @param Item $quoteItem
+     * @param array $data
+     * @return Item
+     */
+    public function setQuoteItemAlmaInsurance(Item $quoteItem, array $data): Item
+    {
+        return $quoteItem->setAlmaInsurance($this->json->serialize($data));
+    }
+
+}
