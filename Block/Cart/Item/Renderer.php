7c462531e291e51932630cda696513cbe74ec6fb
Francois-Gomis <francois.gomis@getalma.eu>
2023-11-28 15:27:44 +0100

    fix: add templates for insurance

---
+++ Block/Cart/Item/Renderer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Alma\MonthlyPayments\Block\Cart\Item;
+
+use Alma\MonthlyPayments\Helpers\InsuranceHelper;
+
+class Renderer extends \Magento\Checkout\Block\Cart\Item\Renderer
+{
+    /**
+     * Get item product name
+     *
+     * @return string
+     */
+    public function getProductName()
+    {
+        if ($this->hasProductName()) {
+            return $this->getData('product_name');
+        }
+        $almaInsurance = json_decode($this->_item->getData(InsuranceHelper::ALMA_INSURANCE_SKU), true);
+        return $this->getProduct()->getName() . ' - ' . $almaInsurance['name'] . ' - ' . $almaInsurance['parent_name'];
+    }
+}
