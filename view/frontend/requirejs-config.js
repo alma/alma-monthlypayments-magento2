4912a246c636b5bf5eb4af64f187555f41ace685
Francois-Gomis <francois.gomis@getalma.eu>
2023-11-29 16:08:34 +0100

    fix: new minicart item renderer

--- view/frontend/requirejs-config.js
+++
@@ -1,7 +0,0 @@
-var config = {
-    map: {
-        "*": {
-            'Magento_Checkout/template/minicart/item/default.html': 'Alma_MonthlyPayments/template/minicart/item/default.html'
-        }
-    }
-};
