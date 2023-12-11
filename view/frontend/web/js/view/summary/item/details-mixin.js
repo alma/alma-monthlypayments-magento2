92d2bc72b386753a1af0b300d28fd5d5bbfda00f
Francois-Gomis <francois.gomis@getalma.eu>
2023-12-11 12:31:27 +0100

    feature: Change summary product Name

---
+++ view/frontend/web/js/view/summary/item/details-mixin.js
@@ -0,0 +1,29 @@
+define([
+    'Magento_Customer/js/customer-data'
+], function (customerData) {
+    'use strict';
+
+    var mixin = {
+
+        /**
+         *
+         * @param {Object} quoteItem
+         * @return {String}
+         */
+        getNameUnsanitizedHtml: function (quoteItem) {
+            var carItems = customerData.get('cart')().items;
+            for (let item of carItems){
+                if(item.item_id === quoteItem.item_id) {
+                    if (item.isInsuranceProduct){
+                        return item.product_name;
+                    }
+                    return this._super();
+                }
+            }
+        }
+    };
+
+    return function (target) {
+        return target.extend(mixin);
+    };
+});
