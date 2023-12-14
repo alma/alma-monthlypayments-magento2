ad5263e7f233876d7adfc07b1e21417cec398798
Francois-Gomis <francois.gomis@getalma.eu>
2023-12-14 16:40:38 +0100

    refactor : price in cent front

---
+++ view/frontend/web/js/model/front-price-calculation.js
@@ -0,0 +1,40 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+define([
+    'jquery',
+    'mage/validation'
+], function ($) {
+    'use strict';
+
+    var agreementsInputPath = '.payment-method._active .alma-validation-input input';
+
+    /**
+     *
+     * @param   {string} priceHtml The price extracted from HTML
+     * @returns {number} 1 for flaot or 100 for integer
+     */
+    function getCentMultiplier (priceHtml){
+        var multiplier = 1;
+        var countSeparator = priceHtml.match(/[.,]/g) || [];
+        if (countSeparator.length == 0 || (countSeparator.length == 1 && (/[.,][\d]{3}/g).test(priceHtml))){
+            multiplier = 100;
+        }
+        return multiplier;
+    }
+    return {
+
+        /**
+         *
+         * @param priceHtml The price extracted from HTML
+         * @returns {number} price in cent
+         */
+        convertHtmlPriceToCent : function (priceHtml){
+        var centMultiplier = getCentMultiplier(priceHtml);
+        var price = priceHtml.replace(/[^\d]/g,"");
+        return price * centMultiplier;
+        },
+    };
+});
