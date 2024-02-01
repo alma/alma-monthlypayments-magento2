643c9085afc79cfb5f16dfbd21ea98c1a9fc0247
Francois-Gomis <francois.gomis@getalma.eu>
2024-02-01 15:06:14 +0100

    feature : format price by currency

---
+++ Ui/Component/Insurance/Listing/Column/SubscriptionAmount.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Alma\MonthlyPayments\Ui\Component\Insurance\Listing\Column;
+
+use Alma\MonthlyPayments\Helpers\Functions;
+use Magento\Framework\Pricing\PriceCurrencyInterface;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Sales\Model\OrderRepository;
+use Magento\Ui\Component\Listing\Columns\Column;
+
+class SubscriptionAmount extends Column
+{
+
+    /**
+     * @var OrderRepository
+     */
+    private $orderRepository;
+    /**
+     * @var PriceCurrencyInterface
+     */
+    private $priceCurrency;
+
+    public function __construct(
+        ContextInterface       $context,
+        UiComponentFactory     $uiComponentFactory,
+        OrderRepository        $orderRepository,
+        PriceCurrencyInterface $priceCurrency,
+        array                  $components = [],
+        array                  $data = []
+    ) {
+        parent::__construct(
+            $context,
+            $uiComponentFactory,
+            $components,
+            $data
+        );
+        $this->orderRepository = $orderRepository;
+        $this->priceCurrency = $priceCurrency;
+    }
+
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as & $item) {
+                if ($item) {
+                    $order = $this->orderRepository->get($item['order_id']);
+                    $currency = $order->getOrderCurrencyCode();
+                    $item['subscription_amount'] = $this->priceCurrency->convertAndFormat(Functions::priceFromCents($item['subscription_amount']), false, 2, null, $currency);
+                }
+            }
+        }
+
+        return $dataSource;
+    }
+}
