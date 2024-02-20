75ee180606927328f8309962c365f7e978b8e258
Francois-Gomis <francois.gomis@getalma.eu>
2024-02-20 11:11:18 +0100

    feature : Send notification in admin BO for canceled subscription

--- Model/Api/Insurance/InsuranceUpdate.php
+++ Model/Api/Insurance/InsuranceUpdate.php
@@ -8,9 +8,12 @@ use Alma\MonthlyPayments\Helpers\AlmaClient;
 use Alma\MonthlyPayments\Helpers\Logger;
 use Alma\MonthlyPayments\Model\Insurance\ResourceModel\Subscription;
 use Alma\MonthlyPayments\Model\Insurance\ResourceModel\Subscription\CollectionFactory;
+use Magento\Backend\Model\Url;
 use Magento\Framework\Exception\AlreadyExistsException;
+use Magento\Framework\Notification\NotifierPool;
 use Magento\Framework\Webapi\Exception;
 use Magento\Framework\Webapi\Rest\Request;
+use Magento\Sales\Model\OrderRepository;
 
 class InsuranceUpdate implements InsuranceUpdateInterface
 {
@@ -25,6 +28,9 @@ class InsuranceUpdate implements InsuranceUpdateInterface
     private $almaClient;
     private CollectionFactory $subscriptionCollection;
     private Subscription $subscription;
+    private NotifierPool $notifierPool;
+    private OrderRepository $orderRepository;
+    private Url $url;
 
     /**
      * @param Request $request
@@ -35,20 +41,27 @@ class InsuranceUpdate implements InsuranceUpdateInterface
         Logger            $logger,
         AlmaClient        $almaClient,
         CollectionFactory $subscriptionCollection,
-        Subscription      $subscription
-    ) {
+        Subscription      $subscription,
+        NotifierPool      $notifierPool,
+        OrderRepository   $orderRepository,
+        Url               $url
+    )
+    {
         $this->request = $request;
         $this->logger = $logger;
         $this->almaClient = $almaClient;
         $this->subscriptionCollection = $subscriptionCollection;
         $this->subscription = $subscription;
+        $this->notifierPool = $notifierPool;
+        $this->orderRepository = $orderRepository;
+        $this->url = $url;
     }
 
     /**
      * @return string
      * @throws Exception
      */
-    public function update():void
+    public function update(): void
     {
         $params = $this->request->getParams();
         $this->checkQeuryParams($params);
@@ -64,8 +77,16 @@ class InsuranceUpdate implements InsuranceUpdateInterface
         $dbSubscription->setSubscriptionBrokerId($subscription['broker_subscription_id']);
         try {
             $this->subscription->save($dbSubscription);
-        } catch (AlreadyExistsException | \Exception $e) {
+        } catch (AlreadyExistsException|\Exception $e) {
             throw new Exception(__('Impossible to save subscription data'), 0, 500);
+        }
+        if (\Alma\API\Entities\Insurance\Subscription::STATE_CANCELLED === $subscription['state']) {
+            $order = $this->orderRepository->get($dbSubscription->getOrderId());
+            $this->notifierPool->addMajor(
+                __(sprintf('Alma Insurance: Order %s - Cancelled insurance subscriptions need to be refunded', $order->getIncrementId())),
+                sprintf(__('<p>Please be informed that one or multiple subscriptions in Order %s have been canceled.</p><br/><b><p>**Action Required: Refund the customer for the affected subscriptions.**</p></b><br/><p>Thank you.</p>'), $order->getIncrementId()),
+                $this->url->getUrl('sales/order/view', ['order_id' => $order->getId()])
+            );
         }
     }
 
@@ -115,7 +136,7 @@ class InsuranceUpdate implements InsuranceUpdateInterface
             $subscriptions = $this->almaClient->getDefaultClient()->insurance->getSubscription(['id' => $subscriptionId]);
         } catch (AlmaException $e) {
             $this->logger->error("Error getting subscription:", [$e->getMessage()]);
-            throw new Exception(__('Impossible to get subscription_id'), 0, 404, );
+            throw new Exception(__('Impossible to get subscription_id'), 0, 404,);
         }
         return $subscriptions;
     }
