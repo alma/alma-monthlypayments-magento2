b2f08790aff889befad83ecc3e6d86525e7e8914
Francois-Gomis <francois.gomis@getalma.eu>
2024-09-11 09:44:04 +0200

    feat: add disable product method in ProductHelper

--- Test/Unit/Helpers/ProductHelperTest.php
+++ Test/Unit/Helpers/ProductHelperTest.php
@@ -4,10 +4,17 @@ namespace Alma\MonthlyPayments\Test\Unit\Helpers;
 
 use Alma\MonthlyPayments\Helpers\Logger;
 use Alma\MonthlyPayments\Helpers\ProductHelper;
+use Alma\MonthlyPayments\Model\Exceptions\AlmaProductException;
 use Magento\Catalog\Model\Product;
+use Magento\Catalog\Model\Product\Attribute\Source\Status;
+use Magento\Catalog\Model\ProductRepository;
 use Magento\Catalog\Model\ResourceModel\Category\CollectionFactory as CategoryCollectionFactory;
 use Magento\Catalog\Model\ResourceModel\Product\Collection;
 use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
+use Magento\Framework\Exception\CouldNotSaveException;
+use Magento\Framework\Exception\InputException;
+use Magento\Framework\Exception\StateException;
+use Magento\Framework\Phrase;
 use PHPUnit\Framework\TestCase;
 
 class ProductHelperTest extends TestCase
@@ -24,12 +31,17 @@ class ProductHelperTest extends TestCase
      * @var CategoryCollectionFactory
      */
     private $categoryCollection;
+    /**
+     * @var ProductRepository
+     */
+    private $productRepository;
 
     public function setUp(): void
     {
         $this->logger = $this->createMock(Logger::class);
         $this->productCollectionFactory = $this->createMock(CollectionFactory::class);
         $this->categoryCollection = $this->createMock(CategoryCollectionFactory::class);
+        $this->productRepository = $this->createMock(ProductRepository::class);
     }
 
     private function createProductHelper(): ProductHelper
@@ -42,7 +54,8 @@ class ProductHelperTest extends TestCase
         return [
             $this->logger,
             $this->productCollectionFactory,
-            $this->categoryCollection
+            $this->categoryCollection,
+            $this->productRepository
         ];
     }
 
@@ -59,5 +72,37 @@ class ProductHelperTest extends TestCase
         $productCollectionMock->method('getIterator')->willReturn($iterator);
         $result = $this->createProductHelper()->getProductsCategoriesIds($productCollectionMock);
         $this->assertEquals(["2", "3", "5"], $result);
+    }
+
+    public function testDisableProductCallSetStatusAndNotThrowExceptionForSave(): void
+    {
+        $product = $this->createMock(Product::class);
+        $product->expects($this->once())->method('setStatus')->with(Status::STATUS_DISABLED);
+        $this->productRepository->expects($this->once())->method('save');
+        $this->createProductHelper()->disableProduct($product);
+    }
+
+    /**
+     * @dataProvider errorDataProvider
+     */
+    public function testDisableProductCallSetStatusAndThrowExceptionForSave($exceptionClass): void
+    {
+        $product = $this->createMock(Product::class);
+        $product->expects($this->once())->method('setStatus')->with(Status::STATUS_DISABLED);
+        $this->productRepository
+            ->expects($this->once())
+            ->method('save')
+            ->willThrowException($exceptionClass);
+        $this->expectException(AlmaProductException::class);
+        $this->createProductHelper()->disableProduct($product);
+    }
+
+    private function errorDataProvider(): array
+    {
+        return [
+            'Could not save Exception' => [new CouldNotSaveException(new Phrase('Error could not save'))],
+            'Input Exception' => [new InputException(new Phrase('Error input exception'))],
+            'State Exception' => [new StateException(new Phrase('Error state exception'))],
+        ];
     }
 }
