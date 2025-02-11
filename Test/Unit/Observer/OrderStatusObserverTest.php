7a72a05f024ccd627745c4894a232aa436344dda
Francois-Gomis <francois.gomis@getalma.eu>
2025-02-11 15:34:52 +0100

    feat: Use the new endpoint for order status

--- Test/Unit/Observer/OrderStatusObserverTest.php
+++ Test/Unit/Observer/OrderStatusObserverTest.php
@@ -21,17 +21,14 @@ class OrderStatusObserverTest extends TestCase
     private $observer;
 
     private $event;
-    private $ordersEndpoint;
     private $paymentsEndpoint;
     private $almaClient;
 
     protected function setUp(): void
     {
-        $this->ordersEndpoint = $this->createMock(Orders::class);
         $this->paymentsEndpoint = $this->createMock(Payments::class);
 
         $client = $this->createMock(Client::class);
-        $client->orders = $this->ordersEndpoint;
         $client->payments = $this->paymentsEndpoint;
 
         $this->logger = $this->createMock(Logger::class);
@@ -57,7 +54,7 @@ class OrderStatusObserverTest extends TestCase
         $order->method('getState')->willReturn('new');
         $orderStatusObserver = $this->createOrderStatusObserverObject();
         $this->event->method('getData')->willReturn($order);
-        $this->ordersEndpoint->expects($this->never())->method('sendStatus');
+        $this->paymentsEndpoint->expects($this->never())->method('addOrderStatusByMerchantOrderReference');
         $orderStatusObserver->execute($this->observer);
     }
 
@@ -72,11 +69,11 @@ class OrderStatusObserverTest extends TestCase
 
         $orderStatusObserver = $this->createOrderStatusObserverObject();
         $this->event->method('getData')->willReturn($order);
-        $this->ordersEndpoint->expects($this->never())->method('sendStatus');
+        $this->paymentsEndpoint->expects($this->never())->method('addOrderStatusByMerchantOrderReference');
         $orderStatusObserver->execute($this->observer);
     }
 
-    public function testGivenNoAlmaPaymentIdInAdditionalInformationMustReturnWithoutCallAlmaPayment(): void
+    public function testGivenNoAlmaPaymentIdInAdditionalInformationMustReturnWithoutCallAddOrderStatusByMerchantOrderReference(): void
     {
         $payment = $this->createMock(\Magento\Sales\Model\Order\Payment::class);
         $payment->method('getMethod')->willReturn(Config::CODE);
@@ -89,56 +86,11 @@ class OrderStatusObserverTest extends TestCase
 
         $this->event->method('getData')->willReturn($order);
 
-
-        $this->paymentsEndpoint->expects($this->never())->method('fetch');
+        $this->paymentsEndpoint->expects($this->never())->method('addOrderStatusByMerchantOrderReference');
         $this->createOrderStatusObserverObject()->execute($this->observer);
 
     }
 
-    public function testGivenAnAlmaPaymentWithoutOrdersShouldNotCallSendStatus(): void
-    {
-        $payment = $this->createMock(\Magento\Sales\Model\Order\Payment::class);
-        $payment->method('getMethod')->willReturn(Config::CODE);
-        $payment->method('getAdditionalInformation')->willReturn([Config::ORDER_PAYMENT_ID => 'alma_payment_external_id']);
-
-        $order = $this->createMock(\Magento\Sales\Model\Order::class);
-        $order->method('getState')->willReturn('processing');
-        $order->method('getPayment')->willReturn($payment);
-        $order->method('getIncrementId')->willReturn('100000013');
-
-        $this->event->method('getData')->willReturn($order);
-        $almaPayment = $this->createMock(Payment::class);
-        $almaPayment->orders = [];
-
-        $this->paymentsEndpoint->expects($this->once())->method('fetch')->with('alma_payment_external_id')->willReturn($almaPayment);
-        $this->ordersEndpoint->expects($this->never())->method('sendStatus');
-        $this->createOrderStatusObserverObject()->execute($this->observer);
-    }
-
-    public function testGivenAnAlmaPaymentWithOrdersWithBadMerchantReferenceShouldNotCallSendStatus(): void
-    {
-        $payment = $this->createMock(\Magento\Sales\Model\Order\Payment::class);
-        $payment->method('getMethod')->willReturn(Config::CODE);
-        $payment->method('getAdditionalInformation')->willReturn([Config::ORDER_PAYMENT_ID => 'alma_payment_external_id']);
-
-        $order = $this->createMock(\Magento\Sales\Model\Order::class);
-        $order->method('getState')->willReturn('processing');
-        $order->method('getPayment')->willReturn($payment);
-        $order->method('getIncrementId')->willReturn('100000013');
-
-        $this->event->method('getData')->willReturn($order);
-        $almaPayment = $this->createMock(Payment::class);
-
-        $almaOrder = $this->createMock(Order::class);
-        $almaOrder->merchant_reference = '100000012';
-        $almaOrder2 = $this->createMock(Order::class);
-        $almaOrder2->merchant_reference = '100000014';
-        $almaPayment->orders = [$almaOrder, $almaOrder2];
-
-        $this->paymentsEndpoint->expects($this->once())->method('fetch')->with('alma_payment_external_id')->willReturn($almaPayment);
-        $this->ordersEndpoint->expects($this->never())->method('sendStatus');
-        $this->createOrderStatusObserverObject()->execute($this->observer);
-    }
 
     /**
      * @dataProvider orderStatusDataProvider
@@ -158,14 +110,17 @@ class OrderStatusObserverTest extends TestCase
 
         $this->event->method('getData')->willReturn($order);
 
-        $almaOrder = $this->createMock(Order::class);
-        $almaOrder->id = 'alma_order_external_id';
-        $almaOrder->merchant_reference = '100000013';
-        $almaPayment = $this->createMock(Payment::class);
-        $almaPayment->orders = [$almaOrder];
+        $this->paymentsEndpoint->expects($this->never())->method('fetch');
+        $this->paymentsEndpoint
+            ->expects($this->once())
+            ->method('addOrderStatusByMerchantOrderReference')
+            ->with(
+                'alma_payment_external_id',
+                '100000013',
+                $dataProvider['status'],
+                $dataProvider['is_shipped']
 
-        $this->paymentsEndpoint->expects($this->once())->method('fetch')->with('alma_payment_external_id')->willReturn($almaPayment);
-        $this->ordersEndpoint->expects($this->once())->method('sendStatus')->with('alma_order_external_id', ['status' => $dataProvider['status'], 'is_shipped' => $dataProvider['is_shipped']]);
+            );
         $this->createOrderStatusObserverObject()->execute($this->observer);
     }
 
