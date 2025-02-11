210e8c269a006173684cbb2c3aa86c43a8094a49
Francois-Gomis <francois.gomis@getalma.eu>
2025-02-11 18:23:55 +0100

    feat: Add throw exception unit testing

--- Test/Unit/Observer/OrderStatusObserverTest.php
+++ Test/Unit/Observer/OrderStatusObserverTest.php
@@ -7,8 +7,10 @@ use Alma\API\Endpoints\Orders;
 use Alma\API\Endpoints\Payments;
 use Alma\API\Entities\Order;
 use Alma\API\Entities\Payment;
+use Alma\API\Exceptions\RequestException;
 use Alma\MonthlyPayments\Gateway\Config\Config;
 use Alma\MonthlyPayments\Helpers\AlmaClient;
+use Alma\MonthlyPayments\Helpers\Exceptions\AlmaClientException;
 use Alma\MonthlyPayments\Helpers\Logger;
 use Alma\MonthlyPayments\Observer\OrderStatusObserver;
 use Magento\Framework\Event;
@@ -91,6 +93,49 @@ class OrderStatusObserverTest extends TestCase
 
     }
 
+    public function testGivenAlmaPaymentClientThrowExceptionReturnVoidAndLog(): void
+    {
+        $payment = $this->createMock(\Magento\Sales\Model\Order\Payment::class);
+        $payment->method('getMethod')->willReturn(Config::CODE);
+        $payment->method('getAdditionalInformation')->willReturn([Config::ORDER_PAYMENT_ID => 'alma_payment_external_id']);
+
+        $order = $this->createMock(\Magento\Sales\Model\Order::class);
+        $order->method('getState')->willReturn('processing');
+        $order->method('getStatus')->willReturn('processing_status');
+        $order->method('getPayment')->willReturn($payment);
+
+        $this->event->method('getData')->willReturn($order);
+        $this->almaClient->method('getDefaultClient')->willThrowException(new AlmaClientException('error'));
+        $this->paymentsEndpoint->expects($this->never())->method('addOrderStatusByMerchantOrderReference');
+        $this->logger
+            ->expects($this->once())
+            ->method('error');
+        $this->createOrderStatusObserverObject()->execute($this->observer);
+
+    }
+
+    public function testGivenAlmaPaymentAddOrderStatusThrowExceptionReturnVoidAndLog(): void
+    {
+        $payment = $this->createMock(\Magento\Sales\Model\Order\Payment::class);
+        $payment->method('getMethod')->willReturn(Config::CODE);
+        $payment->method('getAdditionalInformation')->willReturn([Config::ORDER_PAYMENT_ID => 'alma_payment_external_id']);
+
+        $order = $this->createMock(\Magento\Sales\Model\Order::class);
+        $order->method('getState')->willReturn('processing');
+        $order->method('getStatus')->willReturn('processing_status');
+        $order->method('getPayment')->willReturn($payment);
+
+        $this->event->method('getData')->willReturn($order);
+        $this->paymentsEndpoint
+            ->expects($this->once())
+            ->method('addOrderStatusByMerchantOrderReference')
+            ->willThrowException(new RequestException('error'));
+        $this->logger
+            ->expects($this->once())
+            ->method('error');
+        $this->createOrderStatusObserverObject()->execute($this->observer);
+
+    }
 
     /**
      * @dataProvider orderStatusDataProvider
