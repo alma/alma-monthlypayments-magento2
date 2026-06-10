77c78758d8ff09d4ae535eed7a8676adce0c2504
Jimmy Carricart <jimmy.carricart-ext@getalma.eu>
2026-06-10 17:35:31 +0200

    fix(webapi): fix swagger schema generation for collect endpoint

--- Api/Data/CollectDataInterface.php
+++ Api/Data/CollectDataInterface.php
@@ -2,13 +2,11 @@
 
 namespace Alma\MonthlyPayments\Api\Data;
 
-use Magento\Framework\App\ResponseInterface;
-
 interface CollectDataInterface
 {
     /**
      * Collect configuration data Api return void value but an HTTP response is sent to the client
-     * @return ResponseInterface
+     * @return void
      */
     public function collect();
 }
